8dc0d10936cec1eb25f534831e381ae1237b8cad
Grazielle Cafe <grazielle.cafe@aluno.ufop.edu.br>
2022-12-18 21:23:01 -0300

    feat: create tests for checkbox item

--- color-button-project/src/App.test.js
+++ color-button-project/src/App.test.js
@@ -5,14 +5,20 @@ test('button has correct initial color and text and update when clicked', () => 
   render(<App />); 
   const colorButton = screen.getByRole('button', { name: 'Change to blue' }); 
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' }); 
-});
-
-test('button turns blue when clicked', () => {
-  render(<App />);
-  const colorButton = screen.getByRole('button', { name: 'Change to blue' }); 
 
   fireEvent.click(colorButton); 
 
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' }); 
   expect(colorButton).toHaveTextContent('Change to red'); 
+})
+
+test('initial conditions', () => {
+  render(<App />)
+  // check that the button starts out enabled 
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' }); 
+  expect(colorButton).toBeEnabled(); 
+
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox'); 
+  expect(checkbox).not.toBeChecked(); 
 })
